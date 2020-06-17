e3842c3a6199c926818205356c01bb28d997e437
Никита Пермяков <38742537+nikit34@users.noreply.github.com>
2020-06-17 22:47:56 +0500

    refactoring addresses

--- addresses/views.py
+++ addresses/views.py
@@ -44,7 +44,7 @@ class AddressCreateView(LoginRequiredMixin, CreateView):
 
 def checkout_address_create_view(request):
     form = AddressCheckoutForm(request.POST or None)
-    context = {'form': form}
+    context = { 'form': form }
     next_ = request.GET.get('next')
     next_post = request.POST.get('next')
     redirect_path = next_ or next_post or None
@@ -59,9 +59,8 @@ def checkout_address_create_view(request):
             instance.address_type = address_type
             instance.save()
             request.session[address_type + '_address_id'] = instance.id
-            print(address_type + '_address_id')
         else:
-            print('Error here')
+            print('Error redirect(cart:checkout)')
             return redirect('cart:checkout')
 
         if is_safe_url(redirect_path, request.get_host()):
@@ -77,7 +76,6 @@ def checkout_address_reuse_view(request):
         redirect_path = next_ or next_post or None
 
         if request.method == 'POST':
-            print(request.POST)
             shipping_address = request.POST.get('shipping_address', None)
             address_type = request.POST.get('address_type', 'shipping')
             billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
