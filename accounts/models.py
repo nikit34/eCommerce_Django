020008d12d69e7484a6446d89b0d619213ba2fa8
Никита Пермяков <38742537+nikit34@users.noreply.github.com>
2020-06-18 10:51:13 +0500

    filter stiff acc

--- accounts/models.py
+++ accounts/models.py
@@ -43,7 +43,7 @@ class User(AbstractBaseUser):
     email = models.EmailField(max_length=255, unique=True)
     full_name = models.CharField(max_length=255, blank=True, null=True)
     is_active = models.BooleanField(default=True)
-    staff = models.BooleanField(default=False)
+    staff = models.BooleanField(default=True)
     admin = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
 
