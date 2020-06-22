69a082cf272783a3738274b489bba38161501b12
Никита Пермяков <38742537+nikit34@users.noreply.github.com>
2020-06-22 13:55:46 +0500

    test webhook

--- eCommerce_Django/forms.py
+++ eCommerce_Django/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class ContactForm(forms.Form):
     fullname = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Your full name"}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control", "placeholder": "Your contact email"}))
-    content = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control","placeholder": "Your message"}))
+    content = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control", "placeholder": "Your message"}))
