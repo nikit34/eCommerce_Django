c18af6d08a0d10dfd87424014eed61851a8e4bb4
Никита Пермяков <38742537+nikit34@users.noreply.github.com>
2020-06-22 10:25:51 +0500

    u

--- eCommerce_Django/wsgi.py
+++ eCommerce_Django/wsgi.py
@@ -1,11 +1,11 @@
 import os
 import sys
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 path = os.path.expanduser('/home/OlyaStudio/eCommerce_Django/')
 if path not in sys.path:
     sys.path.insert(0, path)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'eCommerce_Django.settings'
 
-from django.contrib.staticfiles.handlers import StaticFilesHandler
 application = StaticFilesHandler(get_wsgi_application())
