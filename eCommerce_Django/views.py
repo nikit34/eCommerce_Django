17d59cc83a9ca7b9d0797d9bd2cb15afa3b24a54
Никита Пермяков <38742537+nikit34@users.noreply.github.com>
2020-06-23 16:09:57 +0500

    enumeration of options

--- eCommerce_Django/views.py
+++ eCommerce_Django/views.py
@@ -75,7 +75,7 @@ def contact_page(request):
 @csrf_exempt
 def update(request):
     if request.method == 'POST':
-        repo = git.Repo("/home/OlyaStudio/", search_parent_directories=True)
+        repo = git.Repo("git@github.com:nikit34/eCommerce_Django.git")
         origin = repo.remotes.origin
         origin.pull()
         return HttpResponse("Update code on server")
