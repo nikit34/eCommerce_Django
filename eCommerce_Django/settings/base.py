cdef9c6ba6eb2c1c53c6d4fca41aa60a52946855
nikit34 <permikov134@mail.ru>
2020-07-04 10:47:57 +0500

    Revert "add footer"
    This reverts commit a9a5507b42b55b17183bf0494d4e450a5ea842a2.

--- eCommerce_Django/settings/base.py
+++ eCommerce_Django/settings/base.py
@@ -69,7 +69,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
 ]
 
 
