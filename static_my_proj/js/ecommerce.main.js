f3b0b88de423a08af4d63063213c883afbc37f4b
nikit34 <permikov134@mail.ru>
2020-07-08 16:59:07 +0500

    add getCookie - need TODO

--- static_my_proj/js/ecommerce.main.js
+++ static_my_proj/js/ecommerce.main.js
@@ -156,31 +156,52 @@ $(document).ready(function () {
   }
 
 
-  paypal.Buttons({
-
-    style: {
-        color:  'blue',
-        shape:  'pill',
-        label:  'pay',
-        height: 40
-    },
-
-    createOrder: function(data, actions) {
-      return actions.order.create({
-          purchase_units: [{
-              amount: {
-                  value: '0.01'
-              }
-          }]
-      });
-  },
-  onApprove: function(data, actions) {
-      return actions.order.capture().then(function(details) {
-          // Show a success message to the buyer
-          alert('Transaction completed by ' + details.payer.name.given_name + '!');
-      });
+  // PayPal
+  function getCookie(name){
+    let cookieValue = null;
+    if(document.cookie && document.cookie !== ""){
+      let cookies = document.cookie.split(";");
+      for(let i = 0; i < cookies.length; i++){
+        let cookie_name = cookies[i].trim().substring(name.length + 1);
+        if(cookie_name === (name + "=")){
+          cookieValue = decodeURIComponent(cookie_name);
+          break;
+        }
+      }
+    }
+    return cookieValue;
   }
 
+  var csrftoken = getCookie("csrftoken");
+  var orderID = "{{ order.id }}";
+  var amount = "{{ order.get_total }}";
+  var url = "{% url 'payment-paypal' %}"; // TODO: -> new paypal_complete(request): -> PaymentView(View):
 
-}).render('#paypal-button-container');
+  paypal.Buttons({
+
+
+    style: {
+      color:  'blue',
+      shape:  'pill',
+      label:  'pay',
+      height: 40
+    },
+    createOrder: function(data, actions) {
+      return actions.order.create({
+        purchase_units: [{
+          amount: {
+            value: '0.02'
+          }
+        }]
+      });
+    },
+    onApprove: function(data, actions) {
+      return actions.order.capture().then(function(details) {
+
+          alert('Transaction completed by ' + details.payer.name.given_name + '!');
+      });
+    }
+  }).render('#paypal-button-container');
+
+
 });
