beb2d7dd91ffbf2e9c7940da8684e3f3e6e09336
nikit34 <permikov134@mail.ru>
2020-07-10 13:44:40 +0500

    d

--- eCommerce_Django/settings/pythonanywhere.py
+++ eCommerce_Django/settings/pythonanywhere.py
@@ -144,3 +144,4 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
 
 PROTECTED_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "protected_media")
+d
