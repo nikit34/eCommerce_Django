e0882f84d8039cbd654bc77047a4b17dcf27090c
nikit34 <permikov134@mail.ru>
2020-07-14 11:53:50 +0500

    basic test models

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,1 +1,85 @@
 from django.test import TestCase
+
+from .models import User, EmailActivation, GuestEmail
+
+class UserModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+
+    def test_email_label(self):
+        user = User.objects.get(id=1)
+        field_label = user._meta.get_field('email').verbose_name
+        self.assertEqual(field_label, 'email')
+
+    def test_full_name_label(self):
+        user = User.objects.get(id=1)
+        field_label = user._meta.get_field('full_name').verbose_name
+        self.assertEqual(field_label, 'full name')
+
+    def test_email_max_length(self):
+        user = User.objects.get(id=1)
+        max_length = user._meta.get_field('email').max_length
+        self.assertEqual(max_length, 255)
+
+    def test_full_name_max_length(self):
+        user = User.objects.get(id=1)
+        max_length = user._meta.get_field('full_name').max_length
+        self.assertEqual(max_length, 255)
+
+    def test_object_name_is_email(self):
+        user = User.objects.get(id=1)
+        expected_object_name = f'{user.email}'
+        self.assertEqual(expected_object_name, str(user))
+
+    def test_get_full_name(self):
+        user = User.objects.get(id=1)
+        self.assertEqual(user.get_full_name(), 'Test')
+
+    def test_short_name(self):
+        user = User.objects.get(id=1)
+        self.assertEqual(user.get_short_name(), 'usermodeltest@gmail.com')
+
+
+class GuestEmailModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        GuestEmail.objects.create(email='guestemailmodeltest@gmail.com')
+
+    def test_email_label(self):
+        guest_email = GuestEmail.objects.get(id=1)
+        field_label = guest_email._meta.get_field('email').verbose_name
+        self.assertEqual(field_label, 'email')
+
+    def test_object_name_is_email(self):
+        guest_email = GuestEmail.objects.get(id=1)
+        expected_object_name = f'{guest_email.email}'
+        self.assertEqual(expected_object_name, str(guest_email))
+
+
+class EmailActivationModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+        EmailActivation.objects.create(
+            user=user,
+            email='emailactivationmodeltest@gmail.com'
+            )
+
+    def test_user_label(self):
+        email_activation = EmailActivation.objects.get(id=1)
+        field_label = email_activation._meta.get_field('user').verbose_name
+        self.assertEqual(field_label, 'user')
+
+    def test_email_label(self):
+        email_activation = EmailActivation.objects.get(id=1)
+        field_label = email_activation._meta.get_field('email').verbose_name
+        self.assertEqual(field_label, 'email')
