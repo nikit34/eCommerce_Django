cca2498967205f729aaf2e9e46503482a904133a
nikit34 <permikov134@mail.ru>
2020-07-15 00:19:06 +0500

    test views done for acc

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,41 +1,269 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from accounts.models import User, EmailActivation, GuestEmail
-from accounts.forms import GuestForm
-from accounts.views import GuestRegisterView
+from accounts.models import User
 
 
 class AccountHomeViewTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        User.objects.create(
+    def setUp(self):
+        test_user = User.objects.create_user(
             email='usermodeltest@gmail.com',
             full_name='Test',
             password='test'
         )
+        test_user.save()
+
+    def test_url_get_not_logged_in(self):
+        response = self.client.get('/en/account/')
+        self.assertRedirects(response, '/login/?next=/en/account/', fetch_redirect_response=False)
 
     def test_url_get(self):
-        response = self.client.get('/en/')
+        login = self.client.login(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+        response = self.client.get('/en/account/')
         self.assertEqual(response.status_code, 200)
 
+    def test_url_accessible_by_name(self):
+        login = self.client.login(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+        response = self.client.get(reverse('accounts:account-home'))
+        self.assertEqual(response.status_code, 200)
 
-class GuestRegisterViewTest(TestCase):
+    def test_correct_template(self):
+        login = self.client.login(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+        response = self.client.get('/en/account/')
+        self.assertTemplateUsed(response, 'accounts/home.html')
+
+    def test_correct_context(self):
+        login = self.client.login(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+        response = self.client.get('/en/account/')
+        self.assertEqual(str(response.context['user']), 'usermodeltest@gmail.com')
+
+
+class AccountEmailActivateViewTest(TestCase):
+    def setUp(self):
+        test_user = User.objects.create_user(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+        test_user.save()
+
+    def test_url_get_not_logged_in(self):
+        response = self.client.get('/en/account/email/resend-activation/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_accessible_by_name_not_logged_in(self):
+        response = self.client.get(reverse('accounts:resend-activation'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_correct_template_not_logged_in(self):
+        response = self.client.get('/en/account/email/resend-activation/')
+        self.assertTemplateUsed(response, 'registration/activation-error.html')
+
     def test_url_get(self):
+        login = self.client.login(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+        response = self.client.get('/en/account/email/resend-activation/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_accessible_by_name(self):
+        login = self.client.login(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+        response = self.client.get(reverse('accounts:resend-activation'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_correct_template(self):
+        login = self.client.login(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+        response = self.client.get('/en/account/email/resend-activation/')
+        self.assertTemplateUsed(response, 'registration/activation-error.html')
+
+
+class LoginViewTest(TestCase):
+    def setUp(self):
+        test_user = User.objects.create_user(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+        test_user.save()
+
+    def test_url_get_not_logged_in(self):
+        response = self.client.get('/en/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_accessible_by_name_not_logged_in(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_correct_template_not_logged_in(self):
+        response = self.client.get('/en/login/')
+        self.assertTemplateUsed(response, 'accounts/login.html')
+
+    def test_url_get(self):
+        login = self.client.login(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+        response = self.client.get('/en/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_accessible_by_name(self):
+        login = self.client.login(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_correct_template(self):
+        login = self.client.login(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+        response = self.client.get('/en/login/')
+        self.assertTemplateUsed(response, 'accounts/login.html')
+
+    def test_correct_context(self):
+        login = self.client.login(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+        response = self.client.get('/en/login/')
+        self.assertEqual(str(response.context['user']), 'usermodeltest@gmail.com')
+
+
+class RegisterViewTest(TestCase):
+    def setUp(self):
+        test_user = User.objects.create_user(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+        test_user.save()
+
+    def test_url_get_not_logged_in(self):
+        response = self.client.get('/en/register/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_accessible_by_name_not_logged_in(self):
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_correct_template_not_logged_in(self):
+        response = self.client.get('/en/register/')
+        self.assertTemplateUsed(response, 'accounts/register.html')
+
+    def test_url_get(self):
+        login = self.client.login(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
         response = self.client.get('/en/register/')
         self.assertEqual(response.status_code, 200)
 
     def test_url_accessible_by_name(self):
+        login = self.client.login(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
         response = self.client.get(reverse('register'))
         self.assertEqual(response.status_code, 200)
 
     def test_correct_template(self):
+        login = self.client.login(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
         response = self.client.get('/en/register/')
         self.assertTemplateUsed(response, 'accounts/register.html')
 
-    def test_form_invalid(self):
+    def test_correct_context(self):
+        login = self.client.login(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
         response = self.client.get('/en/register/')
-        form = GuestForm(response)
-        guest_register_view = GuestRegisterView()
-        invalid = guest_register_view.form_invalid(form)
-        self.assertEqual(invalid.status_code, 302)
+        self.assertEqual(str(response.context['user']), 'usermodeltest@gmail.com')
+
+
+class UserDetailUpdateViewTest(TestCase):
+    def setUp(self):
+        test_user = User.objects.create_user(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+        test_user.save()
+
+    def test_url_get_not_logged_in(self):
+        response = self.client.get('/en/account/details/')
+        self.assertRedirects(response, '/login/?next=/en/account/details/', fetch_redirect_response=False)
+
+    def test_url_get(self):
+        login = self.client.login(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+        response = self.client.get('/en/account/details/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_accessible_by_name(self):
+        login = self.client.login(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+        response = self.client.get(reverse('accounts:user-update'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_correct_template(self):
+        login = self.client.login(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+        response = self.client.get('/en/account/details/')
+        self.assertTemplateUsed(response, 'accounts/detail-update-view.html')
+
+    def test_correct_context(self):
+        login = self.client.login(
+            email='usermodeltest@gmail.com',
+            full_name='Test',
+            password='test'
+        )
+        response = self.client.get('/en/account/details/')
+        self.assertEqual(str(response.context['user']), 'usermodeltest@gmail.com')
