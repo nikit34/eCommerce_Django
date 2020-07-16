9a8cf49aa7803bdcd4eca012241ad1e525912b08
nikit34 <permikov134@mail.ru>
2020-07-16 13:22:03 +0500

    build structured data for response

--- carts/paypal.py
+++ carts/paypal.py
@@ -42,10 +42,12 @@ class PayPalClient:
 
 
 class CreateOrder(PayPalClient):
-    def create_order(self):
+    def create_order(self, data):
         request = OrdersCreateRequest()
         request.prefer('return=representation')
-        request.request_body(self.build_request_body())
+        d = self.build_request_body(data)
+        print(d)
+        request.request_body(d)
         response = self.client.execute(request)
         if settings.DEBUG:
             print('Status Code: ', response.status_code)
@@ -64,96 +66,25 @@ class CreateOrder(PayPalClient):
         return response
 
     @staticmethod
-    def build_request_body():
-        return \
-  {
-    "intent": "CAPTURE",
-    "application_context": {
-      "brand_name": "EXAMPLE INC",
-      "landing_page": "BILLING",
-      "shipping_preference": "SET_PROVIDED_ADDRESS",
-      "user_action": "CONTINUE"
-    },
-    "purchase_units": [
-      {
-        "reference_id": "PUHF",
-        "description": "Sporting Goods",
+    def build_request_body(data):
+        structure = {
+            "intent": "CAPTURE",
+            "application_context": {
+                "brand_name": "OlyalyaStudio",
+                "landing_page": "NO_PREFERENCE",
+                "shipping_preference": "GET_FROM_FILE",
+                "user_action": "PAY_NOW"
+            },
+            "purchase_units": [
+                {
+                    "reference_id": "PUHF",
+                    "description": "PayPal deal",
 
-        "custom_id": "CUST-HighFashions",
-        "soft_descriptor": "HighFashions",
-        "amount": {
-          "currency_code": "USD",
-          "value": "230.00",
-          "breakdown": {
-            "item_total": {
-              "currency_code": "USD",
-              "value": "180.00"
-            },
-            "shipping": {
-              "currency_code": "USD",
-              "value": "30.00"
-            },
-            "handling": {
-              "currency_code": "USD",
-              "value": "10.00"
-            },
-            "tax_total": {
-              "currency_code": "USD",
-              "value": "20.00"
-            },
-            "shipping_discount": {
-              "currency_code": "USD",
-              "value": "10"
-            }
-          }
-        },
-        "items": [
-          {
-            "name": "T-Shirt",
-            "description": "Green XL",
-            "sku": "sku01",
-            "unit_amount": {
-              "currency_code": "USD",
-              "value": "90.00"
-            },
-            "tax": {
-              "currency_code": "USD",
-              "value": "10.00"
-            },
-            "quantity": "1",
-            "category": "PHYSICAL_GOODS"
-          },
-          {
-            "name": "Shoes",
-            "description": "Running, Size 10.5",
-            "sku": "sku02",
-            "unit_amount": {
-              "currency_code": "USD",
-              "value": "45.00"
-            },
-            "tax": {
-              "currency_code": "USD",
-              "value": "5.00"
-            },
-            "quantity": "2",
-            "category": "PHYSICAL_GOODS"
-          }
-        ],
-        "shipping": {
-          "method": "United States Postal Service",
-          "address": {
-            "name": {
-              "full_name":"John",
-              "surname":"Doe"
-            },
-            "address_line_1": "123 Townsend St",
-            "address_line_2": "Floor 6",
-            "admin_area_2": "San Francisco",
-            "admin_area_1": "CA",
-            "postal_code": "94107",
-            "country_code": "US"
-          }
+                    "custom_id": "CUST-HighFashions",
+                    "soft_descriptor": "HighFashions",
+                },
+
+            ]
         }
-      }
-    ]
-  }
+        structure['purchase_units'][0].update(data)
+        return structure
