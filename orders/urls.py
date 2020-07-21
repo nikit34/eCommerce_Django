046d68390ffc159a0b94c1452cbdcf52b408f4b1
nikit34 <permikov134@mail.ru>
2020-07-21 14:35:31 +0500

    blyatL ebanya vnimatellnostL

--- orders/urls.py
+++ orders/urls.py
@@ -4,7 +4,7 @@ from .views import (OrderListView, OrderDetailView, VerifyOwnership)
 
 
 urlpatterns = [
-    url(r'$', OrderListView.as_view(), name='list'),
+    url(r'^$', OrderListView.as_view(), name='list'),
     url(r'^endpoint/verify/ownership/$', VerifyOwnership.as_view(), name='verify-ownership'),
     url(r'^(?P<order_id>[0-9A-Za-z]+)/$', OrderDetailView.as_view(), name='detail'),
 ]
