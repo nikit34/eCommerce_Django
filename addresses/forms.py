c73e1c262bff674833f5b237720222f40b45d0b3
nikit34 <permikov134@mail.ru>
2020-07-22 01:26:02 +0500

    add check interval code countries

--- addresses/forms.py
+++ addresses/forms.py
@@ -1,9 +1,21 @@
 from django import forms
+from django.utils.translation import gettext
 
 from .models import Address
+from .countries import countries_dict
 
 
 class AddressForm(forms.ModelForm):
+    def clean_country(self):
+        country_code = self.cleaned_data.get('country')
+        valid_code = False
+        for country_union in countries_dict:
+            if country_code is country_union['code']:
+                valid_code = True
+        if not valid_code:
+            raise forms.ValidationError(gettext('Country code is not valid'))
+        return country_code
+
     class Meta:
         model = Address
         fields = [
